1a79ecaf1e324366da53510287b2373e9994bf6d
pmijar <Prashanth.Mijar@gmail.com>
2015-08-18 23:18:46 -0400

    Corrected the CacheSolve Function to call the getInverse() function within rather than column

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(Matrix_From_Global = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
 	
 	Matrix_Inverse_To_Return <- NULL # Initialize the Matrix variable that needs to be inversed in Parent Environment to be returned 
 	
 	# setMatrix Function would set the Global Matrix
 	setMatrix <- function (Matrix) {
-		Matrix_From_Global <<- Matrix           # Just an Assignment of the Local Variable Matrix to Parent Variable Matrix.		
+		x <<- Matrix           # Just an Assignment of the Local Variable Matrix to Parent Variable Matrix.		
 		Matrix_Inverse_To_Return <<- NULL     # 
 	}
-	getMatrix <- function () Matrix_From_Global
+	getMatrix <- function () x
 	setInverse <- function(solve) Matrix_Inverse_To_Return <<- solve
 	getInverse <- function() Matrix_Inverse_To_Return
 	
@@ -24,12 +24,12 @@ makeCacheMatrix <- function(Matrix_From_Global = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        Matrix_Inverse_To_Return <- x$getInverse
+        Matrix_Inverse_To_Return <- x$getInverse()
         if(!is.null(Matrix_Inverse_To_Return)) {
                 message("getting cached data")
                 return(Matrix_Inverse_To_Return)
         }
-        data <- x$getMatrix
+        data <- x$getMatrix()
         Matrix_Inverse_To_Return <- solve(data, ...)
         x$setInverse(Matrix_Inverse_To_Return)
         Matrix_Inverse_To_Return 
