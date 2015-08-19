237031bc9b6c4900d7f544c27410afb53db51f8f
pmijar <Prashanth.Mijar@gmail.com>
2015-08-19 18:13:56 -0400

    Formated the comments to fit in single page and avoid horrinzontal scrolling for the user

--- cachematrix.R
+++ cachematrix.R
@@ -6,15 +6,23 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 	
-	Matrix_Inverse_To_Return <- NULL			# Initialize the Matrix variable to NULL Value so that NULL value can be evaluated in cacheSolve function
-							
-	setMatrix <- function (Matrix) {			# setMatrix() Function would set the Global matrix
-		x <<- Matrix          					# Just an Assignment of the Local Variable Matrix to Parent matrix Variable .		
+	# Initialize the Matrix variable to NULL Value so that NULL value can be evaluated in cacheSolve function
+	Matrix_Inverse_To_Return <- NULL			
+	
+	# setMatrix() Function would set the Global matrix						
+	setMatrix <- function (Matrix) {			
+		x <<- Matrix          							
 		Matrix_Inverse_To_Return <<- NULL    	 
 	}
-	getMatrix <- function () x											# getMatrix() function would just return the matrix 	
-	setInverse <- function(solve) Matrix_Inverse_To_Return <<- solve	# setInverse() function would cache or set the inverse matrix
-	getInverse <- function() Matrix_Inverse_To_Return					# getInverse() function would read the inverse matrix 
+	
+	# getMatrix() function would just return the matrix 	
+	getMatrix <- function () x
+	
+	# setInverse() function would cache or set the inverse matrix											
+	setInverse <- function(solve) Matrix_Inverse_To_Return <<- solve
+	
+	# getInverse() function would read the inverse matrix 	
+	getInverse <- function() Matrix_Inverse_To_Return					
 	
 	list(getMatrix = getMatrix, setMatrix = setMatrix, setInverse = setInverse, getInverse = getInverse) # Returns the list for the makeCacheMatrix function
 }
@@ -24,23 +32,43 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        Matrix_Inverse_To_Return <- x$getInverse()      	# The getInverse() function return value is assigned to a variable Matrix_Inverse_To_Return which would be evaluated in next step
-        if(!is.null(Matrix_Inverse_To_Return)) {            	# In this "if" block code Matrix_Inverse_To_Return variable is evaluated to check for NULL value,  
-                message("getting cached data")           	# If there is No NULL value then this indicates that the matrix x was inversed,  and the code would read the value from the locale cache
-                return(Matrix_Inverse_To_Return)            #  rather than re-computing inverse matrix using Solve function and the cached value is directly returned by the function. 
-        }													# # If there is a NULL value then  this indicates that the matrix x was NOT inversed yet, 
-        data <- x$getMatrix()                  				# the getMatrix() function is used to read the Matrix in the x matrix object.
-        Matrix_Inverse_To_Return <- solve(data, ...)      	# This code computes a Inverse of Matrix using the Solve function and assigns to a Locale variable  Matrix_Inverse_To_Return,  
-        x$setInverse(Matrix_Inverse_To_Return)           	# The Matrix_Inverse_To_Return would be the return value for the cacheSolve function.
+        
+        # The getInverse() function return value is assigned to a variable Matrix_Inverse_To_Return which would be evaluated in next step
+        Matrix_Inverse_To_Return <- x$getInverse()  
+        
+        # In this "if" block code Matrix_Inverse_To_Return variable is evaluated to check for NULL value,
+        # If there is No NULL value then this indicates that the matrix x was inversed,  and the code would read the value from the locale cache
+        #  rather than re-computing inverse matrix using Solve function and the cached value is directly returned by the function.
+            	
+        if(!is.null(Matrix_Inverse_To_Return)) {             
+                message("getting cached data")           	
+                return(Matrix_Inverse_To_Return)             
+        }	
+        
+        # If there is a NULL value then  this indicates that the matrix x was NOT inversed yet, 
+        # the getMatrix() function is used to read the Matrix in the x matrix object.  
+        data <- x$getMatrix()    
+        
+        # This code computes a Inverse of Matrix using the Solve function and assigns to a Locale variable  Matrix_Inverse_To_Return, 
+        # The Matrix_Inverse_To_Return would be the return value for the cacheSolve function.             				
+        Matrix_Inverse_To_Return <- solve(data, ...)      	 
+        x$setInverse(Matrix_Inverse_To_Return)           	
         Matrix_Inverse_To_Return 
 }
 
 
-##   Validation of the cachematrix.R program in R prompt
-##  > source("cachematrix.R")									# Source the function file 
-##  > Mat <- matrix (sample(1:27, 9, replace=F), 3,3)			# Initialize a 3 X 3 dimension matrix with sample function that would pick 9 random values between 1 through 27 to Mat variable
-##  > print(Mat)    											# Print the Mat variable to display the matrix created and its values
-##  > Inverse_Matrix <- cacheSolve(makeCacheMatrix(Mat))		# Use makeCacheMatrix with Mat as an argument to set the Matrix and then use the cacheSolve function to compute the Inverse Matrix
-##  > print(Inverse_Matrix)    									# Print the Inverse_Matrix variable to display the inverted matrix values
-##  > Back_Mat <- cacheSolve(makeCacheMatrix(Inverse_Matrix))   # Re-run the same function now with inverted matrix values and assign it to a variable Back_Mat
-##  > print(Back_Mat)											# Print the Back_Mat variable to display the inverted matrix values and validate if Back_Mat matches the Mat matrix.
+#  "Validation of the cachematrix.R program in R prompt"
+# "Source the function file."
+	##  > source("cachematrix.R")	
+# "Initialize a 3 X 3 dimension matrix with sample function that would pick 9 random values between 1 through 27 to Mat variable."								 
+	##  > Mat <- matrix (sample(1:27, 9, replace=F), 3,3)
+# "Print the Mat variable to display the matrix created and its values."			
+	##  > print(Mat)   
+# "Use makeCacheMatrix with Mat as an argument to set the Matrix and then use the cacheSolve function to compute the Inverse Matrix."											
+	##  > Inverse_Matrix <- cacheSolve(makeCacheMatrix(Mat))
+# "Print the Inverse_Matrix variable to display the inverted matrix values."		
+	##  > print(Inverse_Matrix)   
+# "Re-run the same function now with inverted matrix values and assign it to a variable Back_Mat." 									
+	##  > Back_Mat <- cacheSolve(makeCacheMatrix(Inverse_Matrix))  
+# "Print the Back_Mat variable to display the inverted matrix values and validate if Back_Mat matches the Mat matrix."
+	##  > print(Back_Mat)											
